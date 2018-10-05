026eedb240e26786b74c653f069544615c024b97
Alex Sligar <info@gravelroad.co>
2018-10-04 23:30:12 -0700

    Validate payload for requests

--- modules/validations/routes.js
+++ modules/validations/routes.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Joi = require('joi');
+
 const ValidationsController = require('./controller');
 
 const validationsRoutes = {
@@ -10,12 +12,27 @@ const validationsRoutes = {
             {
                 method:'POST',
                 path:'/validationRequest',
-                handler:ValidationsController.post
+                handler:ValidationsController.post,
+                options: {
+                    validate: {
+                        payload: {
+                            address: Joi.string().min(26).max(35).required()
+                        }
+                    }
+                }
             },
             {
                 method: 'POST',
                 path: '/message-signature/validate',
-                handler:ValidationsController.validate
+                handler:ValidationsController.validate,
+                options: {
+                    validate: {
+                        payload: {
+                            address: Joi.string().min(26).max(35).required(),
+                            signature: Joi.string().min(70).max(100).required()
+                        }
+                    }
+                }
             }
         ]);
     }   
